d0dd5588e2c22ad59db3c213f9c682c238428e5e
Shrineel Trivedi <shrineeltrivedi90@gmail.com>
2023-04-27 05:16:00 -0400

    Restructuring Main File

--- Solution0.js
+++ Solution0.js
@@ -1,65 +1,78 @@
 #!/usr/bin/env node
- 
+
 //Reading the input file and placing the data in an array
 const { error } = require('console');
 const fs = require('fs');
 const writeStream = fs.createWriteStream('sorted-names-list');
 const pathName = writeStream.path;
-const {readFileSync} = require('fs');
+const { readFileSync } = require('fs');
 
+//Function to Read File and convert it into an array
 function syncReadFile(filename) {
 
-//Reading the file 
-const contents = readFileSync(filename, 'utf-8');
-//Creating Array and checking validity of input and avoids names out of range
- 
-function isValid(string){
-  const l = string.split(' ').length
-  return l>=1 && l<=4 ;
+  //Reading the file 
+  const contents = readFileSync(filename, 'utf-8');
+  //Creating Array and checking validity of input and avoids names out of range
+
+
+  function isValid(string) {
+    const l = string.split(' ').length
+    return l >= 1 && l <= 4;
+  }
+  const arr = contents.split(/\r?\n/).filter(isValid);
+  //Array created!
+
+  console.log("Original Unsorted List" + JSON.stringify(arr))
+  return arr;
 }
-const arr = contents.split(/\r?\n/).filter(isValid);
-//Array created!
 
-console.log("Original Unsorted List" + JSON.stringify(arr))
-//Sorting Names by splitting the string by Comparison for correct output
+//Function for sorting Names by splitting the string by Comparison for correct output
+function processSyncFile(arr) {
 
-const compareStrings = (a, b) => {
+  const compareStrings = (a, b) => {
     if (a < b) return -1;
     if (a > b) return 1;
-  
+
     return 0;
   }
-  
-const compare = (a, b) => {
+
+  const compare = (a, b) => {
     const splitA = a.split(" ");
     const splitB = b.split(" ");
     const lastA = splitA[splitA.length - 1];
     const lastB = splitB[splitB.length - 1];
-  
+
     return lastA === lastB ?
       compareStrings(splitA[0], splitB[0]) :
       compareStrings(lastA, lastB);
-  
+
 
   }
-console.log(arr.sort(compare));
- 
+  console.log(arr.sort(compare));
+  return arr.sort(compare);
+}
+
+//Writestream function
+function writeSyncFile(arr) {
+
   //printing each line of array seperately
-arr.forEach(value => writeStream.write(`${value}\n`));
-// finish the writestream
-writeStream.on('finished!', () => {
-   console.log(`Succesfully generated file from array ${pathName}`);
-});
-// Error whilst writing stream
-writeStream.on('error while writing the file', (err) => {
+  arr.forEach(value => writeStream.write(`${value}\n`));
+  // finish the writestream
+  writeStream.on('finished!', () => {
+    console.log(`Succesfully generated file from array ${pathName}`);
+  });
+  // Error whilst writing stream
+  writeStream.on('error while writing the file', (err) => {
     console.error(`There is an error writing the file ${pathName} => ${err}`)
-});
-// close the stream
-writeStream.end();
-     
+  });
+  // close the stream
+  writeStream.end();
+
   return arr;
 }
 
-syncReadFile('./unsorted-names-list.txt');
+writeSyncFile(processSyncFile(syncReadFile('./unsorted-names-list.txt')));
 
 module.exports = syncReadFile;
+module.exports = processSyncFile;
+module.exports = writeSyncFile;
--- Solution0.test.js
+++ Solution0.test.js
@@ -1,20 +1,25 @@
 
 
 //Unit Testing
+const { describe } = require("yargs");
 const syncReadFile = require("./Solution0.js");
+const processSyncFile = require("./Solution0.js");
+const writeSyncFile = ("./Solution0.js");
 const myMock = jest.fn();
 console.log(myMock());
-const arr = ['Morten Gamst Penderson','Antonio Di Natale','Rene Alder','Kakhaber Kaladze']
+const arr = ['Morten Gamst Penderson', 'Antonio Di Natale', 'Rene Alder', 'Kakhaber Kaladze']
+const file_found = { "./sorted-names-list": "Sorted List Output Present" }
+const file_not_found = {}
 
 describe("syncReadFile", () => {
     test('1 should result in "List Present"', () => {
         expect(arr[1]).toMatch(/[^A-Za-z]/g);
-      });
+    });
 });
 
 test('Null Test"', () => {
     expect(arr[1001]).toBeNull;
-  });
+});
 
 test("Array input has a specific name", () => {
 
@@ -22,8 +27,25 @@ test("Array input has a specific name", () => {
 
 });
 
+describe("processSyncFile", () => {
+    test('Sorted List after comparison', () => {
+        expect(arr[1]).toMatch('Adonis Julius Archer');
+    });
+});
 
+beforeEach(() => { jest.resetModules(); jest.resetAllMocks(); })
 
+test('No Output Files Found Test', () => {
+    require("fs").writeFileSync(file_found);
+    const spy = jest.spyOn(console, "log");
+    require('./Solution0.js'); expect(spy.mock.calls).toEqual([['File not found']]);
+})
+
+test('Output File Found Test', () => {
+    require("fs").writeFileSync(file_not_found);
+    const spy = jest.spyOn(console, "log"); require('./Solution0.js');
+    expect(spy.mock.calls).toEqual([['File Found']]);
+})
 
 
 
